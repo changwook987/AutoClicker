4db18c07ae1caa0370aca75a642ee90b60344680
changwook987 <changwook987@naver.com>
2024-09-24 16:47:25 +0900

    change design

--- Views/Main.Designer.cs
+++ Views/Main.Designer.cs
@@ -19,13 +19,12 @@ namespace AutoClicker.Views
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 512);
 
             // disallow resize
-            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            // this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
+            // this.MinimizeBox = false;
 
             // always on top
             this.TopMost = true;
@@ -34,38 +33,40 @@ namespace AutoClicker.Views
 
             // label settings
             label = new Label();
-            label.Location = new Point(0, 0);
-            label.Size = new Size(384, 20);
+            label.Dock = DockStyle.Top;
             label.Text = "WAITING........";
-            Controls.Add(label);
 
             // check box
             var left = new CheckBox();
+            left.Dock = DockStyle.Top;
             left.Text = "Left Click";
-            left.Location = new Point(0, 20);
             left.Click += new EventHandler((sender, e) =>
             {
                 dwFlag ^= Constants.MOUSEEVENTF_LEFTDOWN | Constants.MOUSEEVENTF_LEFTUP;
             });
-            Controls.Add(left);
 
             var right = new CheckBox();
+            right.Dock = DockStyle.Top;
             right.Text = "Right Click";
-            right.Location = new Point(0, 40);
             right.Click += new EventHandler((sender, e) =>
             {
                 dwFlag ^= Constants.MOUSEEVENTF_RIGHTDOWN | Constants.MOUSEEVENTF_RIGHTUP;
             });
-            Controls.Add(right);
 
             var middle = new CheckBox();
+            middle.Dock = DockStyle.Top;
             middle.Text = "Middle Click";
-            middle.Location = new Point(0, 60);
             middle.Click += new EventHandler((sender, e) =>
             {
                 dwFlag ^= Constants.MOUSEEVENTF_MIDDLEDOWN | Constants.MOUSEEVENTF_MIDDLEUP;
             });
+
             Controls.Add(middle);
+            Controls.Add(right);
+            Controls.Add(left);
+            Controls.Add(label);
+
+            Padding = new Padding(10);
         }
     }
 }
